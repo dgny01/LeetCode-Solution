bba3d284fc934754e7511ad7cd16616f86aeaf34
Doğanay <149971941+dgny01@users.noreply.github.com>
2025-05-22 18:21:51 +0300

    Add For Solution Fibonacci Numbers

---
+++ leetcode/Fibonacci Number/Fibonacci Number/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+
+    if (n == 0) {
+        cout << 0 << endl;
+        return 0;
+    }
+    if (n == 1) {
+        cout << 1 << endl;
+        return 0;
+    }
+
+    int a = 0, b = 1, c;
+
+    for (int i = 2; i <= n; ++i) {
+        c = a + b;
+     
+        cout << "i: " << i << ", fibonacci: " << c << endl;  // Her adýmda fibonacci sayýsýný yazdýrýr
+        a = b;
+        b = c;
+    }
+
+    cout << "Fibonacci sayisi: " << c << endl;
+
+    return 0;
+}
